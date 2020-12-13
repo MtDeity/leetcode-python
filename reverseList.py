2365870fc174253f9123fa6dd7d7d7a84fe94bad
MtDeity <o8olo853q43@gmail.com>
2020-12-13 16:35:59 +0900

    206. Reverse Linked List
    Recursive

--- reverseList.py
+++ reverseList.py
@@ -5,11 +5,19 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: ListNode) -> ListNode:
-        # iterative
-        prev = None
-        while head:
-            curr = head
-            head = head.next
-            curr.next = prev
-            prev = curr
+        # # Iterative
+        # prev = None
+        # while head:
+        #     curr = head
+        #     head = head.next
+        #     curr.next = prev
+        #     prev = curr
+        # return prev
+
+        # Recursive
+        if head == None or head.next == None:
+            return head
+        prev = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
         return prev
