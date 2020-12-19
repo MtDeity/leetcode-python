7ef859c7710b824793c65a2931bd6ec941488a2a
MtDeity <o8olo853q43@gmail.com>
2020-12-19 21:47:15 +0900

    remove unused variable

--- Array/removeelement.py
+++ Array/removeelement.py
@@ -1,6 +1,6 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        i = j = 0
+        i = 0
         for j in range(len(nums)):
             if nums[j] != val:
                 nums[i] = nums[j]
