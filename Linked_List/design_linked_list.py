72c6f2e99b74ef1fdbc8b38ea66ff6802f724a4c
Satoshi Kamiyama <o8olo853q43@gmail.com>
2021-01-03 07:14:34 +0900

    707. Design Linked List
    Doubly Linked List

--- Linked_List/design_linked_list.py
+++ Linked_List/design_linked_list.py
@@ -1,7 +1,8 @@
-class Node:
-    def __init__(self, val):
+class ListNode:
+    def __init__(self, val: int = 0, prev: ListNode = None, next: ListNode = None):
         self.val = val
-        self.next = None
+        self.prev = prev
+        self.next = next
 
 
 class MyLinkedList:
@@ -19,49 +20,59 @@ class MyLinkedList:
         """
         if index < 0 or index >= self.size or not self.head:
             return -1
-        curr = self.head
+        current = self.head
         for i in range(index):
-            curr = curr.next
-        return curr.val
+            current = current.next
+        return current.val
 
     def addAtHead(self, val: int) -> None:
         """
-        Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list.
+        Add a node of value val before the first element of the linked list.
+        After the insertion, the new node will be the first node of the linked list.
         """
-        node = Node(val)
-        node.next = self.head
-        self.head = node
+        list_node = ListNode(val)
+        list_node.next = self.head
+        if list_node.next:
+            list_node.next.prev = list_node
+        self.head = list_node
         self.size += 1
 
     def addAtTail(self, val: int) -> None:
         """
         Append a node of value val to the last element of the linked list.
         """
-        node = Node(val)
-        curr = self.head
-        if not curr:
-            self.head = node
+        list_node = ListNode(val)
+        current = self.head
+        if not current:
+            self.head = list_node
         else:
-            while curr.next:
-                curr = curr.next
-            curr.next = node
+            while current.next:
+                current = current.next
+            current.next = list_node
+            current.next.prev = current
         self.size += 1
 
     def addAtIndex(self, index: int, val: int) -> None:
         """
-        Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted.
+        Add a node of value val before the index-th node in the linked list.
+        If index equals to the length of linked list,
+        the node will be appended to the end of linked list.
+        If index is greater than the length, the node will not be inserted.
         """
         if index < 0 or index > self.size:
             return
         elif index == 0:
             self.addAtHead(val)
+        elif index == self.size:
+            self.addAtTail(val)
         else:
-            curr = self.head
+            current = self.head
             for i in range(1, index):
-                curr = curr.next
-            node = Node(val)
-            node.next = curr.next
-            curr.next = node
+                current = current.next
+            list_node = ListNode(val)
+            list_node.next = current.next
+            current.next = list_node
+            current.next.next.prev = list_node
             self.size += 1
 
     def deleteAtIndex(self, index: int) -> None:
@@ -71,12 +82,15 @@ class MyLinkedList:
         if index < 0 or index >= self.size:
             return
         elif index == 0:
+            if self.head.next:
+                self.head.next.prev = None
             self.head = self.head.next
         else:
-            curr = self.head
+            current = self.head
             for i in range(1, index):
-                curr = curr.next
-            curr.next = curr.next.next
+                current = current.next
+            current.next.prev = None
+            current.next.next, current.next = None, current.next.next
         self.size -= 1
 
 # Your MyLinkedList object will be instantiated and called as such:
