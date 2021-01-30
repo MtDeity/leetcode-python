1abd8317354176a27b24c63b12ac1c24ecd37648
Satoshi Kamiyama <o8olo853q43@gmail.com>
2021-01-31 00:41:57 +0900

    1704. Determine if String Halves Are Alike

--- String/determine_if_string_halves_are_alike.py
+++ String/determine_if_string_halves_are_alike.py
@@ -1,22 +1,14 @@
 class Solution:
     def halvesAreAlike(self, s: str) -> bool:
-        vowels = ("a", "e", "i", "o", "u", "A", "E", "I", "O", "U")
-        half = len(s) // 2
-        a = s[:half]
-        b = s[half:]
+        vowels = {"a", "e", "i", "o", "u", "A", "E", "I", "O", "U"}
+        length = len(s)
+        a = b = i = 0
+        j = length // 2
 
-        a_map = {}
-        b_map = {}
-        for c in a:
-            a_map[c] = a_map[c] + 1 if c in a_map else 1
-        for c in b:
-            b_map[c] = b_map[c] + 1 if c in b_map else 1
+        while j < length:
+            a += 1 if s[i] in vowels else 0
+            b += 1 if s[j] in vowels else 0
+            i += 1
+            j += 1
 
-        a_count = b_count = 0
-        for vowel in vowels:
-            if vowel in a_map:
-                a_count += a_map[vowel]
-            if vowel in b_map:
-                b_count += b_map[vowel]
-
-        return a_count == b_count
+        return a == b
