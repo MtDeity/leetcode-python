30c477976b2b18a77386e181e6b8e690ed99016f
Satoshi Kamiyama <o8olo853q43@gmail.com>
2021-02-06 23:57:12 +0900

    1436. Destination City

--- String/destination_city.py
+++ String/destination_city.py
@@ -1,12 +1,6 @@
 class Solution:
     def destCity(self, paths: List[List[str]]) -> str:
-        path_map = {}
-        for city_a, city_b in paths:
-            path_map[city_a] = city_b
-        current_city = paths[0][0]
-        while 1:
-            if current_city in path_map:
-                current_city = path_map[current_city]
-            else:
-                break
-        return current_city
+        path_set = set(path[0] for path in paths)
+        for path in paths:
+            if not path[1] in path_set:
+                return path[1]
