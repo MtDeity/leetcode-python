9d935b2e54bd2d64cd5d3fe921d05e00c492844e
Satoshi Kamiyama <o8olo853q43@gmail.com>
2021-02-21 01:24:36 +0900

    897. Increasing Order Search Tree

--- Tree/increasing_order_search_tree.py
+++ Tree/increasing_order_search_tree.py
@@ -5,16 +5,23 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def increasingBST(self, root: TreeNode) -> TreeNode:
-        def inorder(root: TreeNode):
-            if not root:
-                return
-            yield from inorder(root.left)
-            yield root.val
-            yield from inorder(root.right)
+    # def increasingBST(self, root: TreeNode) -> TreeNode:
+        # def inorder(root: TreeNode):
+        #     if not root:
+        #         return
+        #     yield from inorder(root.left)
+        #     yield root.val
+        #     yield from inorder(root.right)
 
-        answer = current = TreeNode()
-        for val in inorder(root):
-            current.right = TreeNode(val)
-            current = current.right
-        return answer.right
+        # answer = current = TreeNode()
+        # for val in inorder(root):
+        #     current.right = TreeNode(val)
+        #     current = current.right
+        # return answer.right
+    def increasingBST(self, root: TreeNode, tail: TreeNode = None) -> TreeNode:
+        if not root:
+            return tail
+        result = self.increasingBST(root.left, root)
+        root.left = None
+        root.right = self.increasingBST(root.right, tail)
+        return result
