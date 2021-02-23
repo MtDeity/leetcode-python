7a2477d6ba99b922c0363950e3c50ca7d320fb26
Satoshi Kamiyama <o8olo853q43@gmail.com>
2021-02-23 12:49:03 +0900

    961. N-Repeated Element in Size 2N Array

---
+++ Hash_Table/n_repeated_element_in_size_2n_array.py
@@ -0,0 +1,7 @@
+class Solution:
+    def repeatedNTimes(self, A: List[int]) -> int:
+        for i in range(2, len(A)):
+            if A[i] == A[i - 1] or A[i] == A[i - 2]:
+                return A[i]
+        return A[0]
+        # return (sum(A) - sum(set(A))) // (len(A) // 2 - 1)
