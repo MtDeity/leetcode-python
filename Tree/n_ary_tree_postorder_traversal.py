cba2d1f9f1e75eddd2db9dcb182f270c402033c0
Satoshi Kamiyama <o8olo853q43@gmail.com>
2021-03-01 23:44:49 +0900

    590. N-ary Tree Postorder Traversal

---
+++ Tree/n_ary_tree_postorder_traversal.py
@@ -0,0 +1,13 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def postorder(self, root: 'Node') -> List[int]:
+        if not root:
+            return []
+        return [j for i in root.children for j in self.postorder(i)] + [root.val]
