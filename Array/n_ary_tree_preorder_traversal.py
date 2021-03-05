1048089a22d4102edddad75e9644a5af987c7fe3
Satoshi Kamiyama <o8olo853q43@gmail.com>
2021-03-06 01:54:06 +0900

    561. Array Partition I

--- Array/n_ary_tree_preorder_traversal.py
+++ Array/n_ary_tree_preorder_traversal.py
@@ -1,3 +1,12 @@
 class Solution:
     def arrayPairSum(self, nums: List[int]) -> int:
-        return sum(sorted(nums)[::2])
+        # return sum(sorted(nums)[::2])
+        limit = 10000
+        counter = [0] * (limit * 2 + 1)
+        for num in nums:
+            counter[num + limit] += 1
+        total = d = 0
+        for i in range(len(counter)):
+            total += (counter[i] - d + 1) // 2 * (i - limit)
+            d = (counter[i] + d) % 2
+        return total
