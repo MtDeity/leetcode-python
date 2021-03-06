1fb3758fb4e857f775c00ddc3b2abff33d0b1f0e
Satoshi Kamiyama <o8olo853q43@gmail.com>
2021-03-07 03:52:34 +0900

    461. Hamming Distance

--- Bit_Manipulation/hamming_distance.py
+++ Bit_Manipulation/hamming_distance.py
@@ -1,3 +1,9 @@
 class Solution:
     def hammingDistance(self, x: int, y: int) -> int:
-        return bin(x^y).count('1')
+        # return bin(x^y).count('1')
+        xor = x ^ y
+        distance = 0
+        while xor:
+            xor &= xor - 1
+            distance += 1
+        return distance
