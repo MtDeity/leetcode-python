57b94e356870a60e34001271e9d3079ccc15bcfe
Satoshi Kamiyama <o8olo853q43@gmail.com>
2021-03-23 05:34:40 +0900

    Merging 2 Packages

---
+++ Hash_Table/get_indices_of_item_wights.py
@@ -0,0 +1,36 @@
+from typing import List, Dict, Tuple
+import unittest
+
+
+def get_indices_of_item_wights(arr: List[int], limit: int) -> List[int]:
+    if len(arr) < 2:
+        return []
+
+    diffs: Dict[int, int] = {}
+    for index, weight in enumerate(arr):
+        if weight in diffs:
+            diff = diffs[weight]
+            return [index, diff]
+        diff = limit - weight
+        diffs[diff] = index
+    return []
+
+
+class Test(unittest.TestCase):
+    test_cases: List[Tuple[List[int], int, List[int]]] = [
+        ([9], 9, []),
+        ([4, 4], 8, [1, 0]),
+        ([4, 4, 1], 5, [2, 1]),
+        ([4, 6, 10, 15, 16], 21, [3, 1]),
+        ([4, 6, 10, 15, 16], 20, [4, 0]),
+        ([12, 6, 7, 14, 19, 3, 0, 25, 40], 7, [6, 2]),
+    ]
+
+    def test_get_indices_of_item_wights(self):
+        for arr, limit, expected in self.test_cases:
+            actual = get_indices_of_item_wights(arr, limit)
+            assert actual == expected
+
+
+if __name__ == "__main__":
+    unittest.main()
