954059595569ab5f9a311a46bb34bb60a2a2e7f8
Satoshi Kamiyama <55924931+MtDeity@users.noreply.github.com>
2021-04-04 01:44:18 +0900

    1814. Count Nice Pairs in an Array

--- Array/count_nice_pairs_in_an_array.py
+++ Array/count_nice_pairs_in_an_array.py
@@ -4,5 +4,5 @@ from typing import Dict, List
 
 class Solution:
     def countNicePairs(self, nums: List[int]) -> int:
-        revs: Dict[int, int] = Counter([n - int(str(n)[::-1]) for n in nums])
-        return sum((v * (v - 1)) // 2 for v in revs.values()) % (10 ** 9 + 7)
+        diffs: Dict[int, int] = Counter([n - int(str(n)[::-1]) for n in nums])
+        return sum((v * (v - 1)) // 2 for v in diffs.values()) % (10 ** 9 + 7)
