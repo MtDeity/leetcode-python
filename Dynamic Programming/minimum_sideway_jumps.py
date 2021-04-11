f3619169430a4ef22760898cd7990df6d96ad086
Satoshi Kamiyama <55924931+MtDeity@users.noreply.github.com>
2021-04-11 19:57:15 +0900

    1824. Minimum Sideway Jumps

---
+++ Dynamic Programming/minimum_sideway_jumps.py
@@ -0,0 +1,17 @@
+from typing import List, Union
+
+
+class Solution:
+    def minSideJumps(self, obstacles: List[int]) -> Union[int, float]:
+        dp: List[Union[int, float]] = [1, 0, 1]
+        for obstacle in obstacles:
+            if obstacle:
+                dp[obstacle - 1] = float('inf')
+            for i in range(3):
+                if obstacle != i + 1:
+                    dp[i] = min(
+                        dp[i],
+                        dp[(i + 1) % 3] + 1,
+                        dp[(i + 2) % 3] + 1
+                    )
+        return min(dp)
