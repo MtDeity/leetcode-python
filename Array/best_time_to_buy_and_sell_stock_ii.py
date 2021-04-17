05c50e5ebd4d44b8c0cbd74e2cfc4b8b57259cd7
Satoshi Kamiyama <55924931+MtDeity@users.noreply.github.com>
2021-04-17 23:31:09 +0900

    122. Best Time to Buy and Sell Stock II

--- Array/best_time_to_buy_and_sell_stock_ii.py
+++ Array/best_time_to_buy_and_sell_stock_ii.py
@@ -4,9 +4,7 @@ from typing import List
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         profit = 0
-        low = prices[0]
-        for price in prices[1:]:
-            if price > low:
-                profit += price - low
-            low = price
+        for i, price in enumerate(prices[1:]):
+            if price > prices[i]:
+                profit += price - prices[i]
         return profit
