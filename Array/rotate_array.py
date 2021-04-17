8ab6be80d58e4ffff631fccc893ef46b74546467
Satoshi Kamiyama <55924931+MtDeity@users.noreply.github.com>
2021-04-18 05:11:32 +0900

    189. Rotate Array

--- Array/rotate_array.py
+++ Array/rotate_array.py
@@ -1,8 +1,12 @@
+from typing import List
+
+
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
-        n = len(nums)
+        n: int = len(nums)
         k %= n
-        start_idx = count = 0
+        start_idx: int = 0
+        count: int = 0
         while count < n:
             cur_idx, prev = start_idx, nums[start_idx]
             while True:
@@ -13,3 +17,15 @@ class Solution:
                 if start_idx == cur_idx:
                     break
             start_idx += 1
+
+    #     self.reverse(nums, 0, length - 1)
+    #     self.reverse(nums, 0, k - 1)
+    #     self.reverse(nums, k, length - 1)
+
+    # def reverse(self, nums: List[int], start: int, end: int) -> None:
+    #     if start >= end:
+    #         return
+    #     while start < end:
+    #         nums[start], nums[end] = nums[end], nums[start]
+    #         start += 1
+    #         end -= 1
