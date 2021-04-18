0bccac5cebc90290c4a09c7c290f5172eb7ab453
Satoshi Kamiyama <55924931+MtDeity@users.noreply.github.com>
2021-04-19 01:33:28 +0900

    19. Remove Nth Node From End of List

--- Linked_List/remove_nth_node_from_end_of_list.py
+++ Linked_List/remove_nth_node_from_end_of_list.py
@@ -1,20 +1,28 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-class Solution:
-    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
-        left = right = head
-        for i in range(n):
-            right = right.next
+from __future__ import annotations
+from typing import Optional
 
+
+class ListNode:
+    def __init__(self, val: int = 0, next: Optional[ListNode] = None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def removeNthFromEnd(self, head: ListNode, n: int) -> Optional[ListNode]:
+        left = right = head
+        for _ in range(n):
+            if not right:
+                raise IndexError
+            right = right.next
         if not right:
             return head.next
-
         while right.next:
-            right = right.next
+            if not left.next:
+                raise TypeError
             left = left.next
+            right = right.next
+        if not left.next:
+            raise TypeError
         left.next = left.next.next
-
         return head
