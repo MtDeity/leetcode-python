afb850619bd7378f9b3c8ddcb6d277b61a520e54
Satoshi Kamiyama <55924931+MtDeity@users.noreply.github.com>
2021-05-09 03:18:51 +0900

    136. Single Number

--- Bit_Manipulation/single_number.py
+++ Bit_Manipulation/single_number.py
@@ -1,10 +1,8 @@
 from typing import List
 from functools import reduce
+from operator import xor
 
 
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        return reduce(self.xor, nums, 0)
-
-    def xor(self, x: int, y: int) -> int:
-        return x ^ y
+        return reduce(xor, nums, 0)
