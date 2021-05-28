bac2041aec44d210813fbea81cf1640b5bc310e6
Satoshi Kamiyama <55924931+MtDeity@users.noreply.github.com>
2021-05-29 03:03:13 +0900

    204. Count Primes

--- Math/count_primes.py
+++ Math/count_primes.py
@@ -1,10 +1,11 @@
 class Solution:
     def countPrimes(self, n: int) -> int:
-        if n < 2:
+        if n < 3:
             return 0
-        s = [1] * n
-        s[0] = s[1] = 0
+        primes = [1] * n
+        primes[0] = primes[1] = 0
         for i in range(2, int(n ** 0.5) + 1):
-            if s[i]:
-                s[i * i:n:i] = [0] * len(s[i * i:n:i])
-        return sum(s)
+            if primes[i]:
+                for j in range(i * i, n, i):
+                    primes[j] = 0
+        return sum(primes)
