7b154ab14fc160306033e85d37ea7b7ea172ebae
Satoshi Kamiyama <55924931+MtDeity@users.noreply.github.com>
2021-06-03 00:39:10 +0900

    190. Reverse Bits

---
+++ Bit_Manipulation/reverse_bits.py
@@ -0,0 +1,8 @@
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        rev = 0
+        for _ in range(32):
+            rev <<= 1
+            rev += n & 1
+            n >>= 1
+        return rev
