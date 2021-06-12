662d45bdcb1eaa19cd8e24f3fe9bfa7df851b0f8
Satoshi Kamiyama <55924931+MtDeity@users.noreply.github.com>
2021-06-13 06:02:43 +0900

    1893. Check if All the Integers in a Range Are Covered

---
+++ Greedy/check_if_all_the_integers_in_a_range_are_covered.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    def isCovered(self,
+                  ranges: List[List[int]],
+                  left: int,
+                  right: int) -> bool:
+        return all(any(start <= i <= end for start, end in ranges)
+                   for i in range(left, right + 1))
