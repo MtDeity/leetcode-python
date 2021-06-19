ff69707b4a21db6810ebdf5356baaed84b3486af
Satoshi Kamiyama <55924931+MtDeity@users.noreply.github.com>
2021-06-19 19:48:05 +0900

    141. Linked List Cycle

--- Linked_List/linked_list_cycle.py
+++ Linked_List/linked_list_cycle.py
@@ -1,15 +1,17 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+from __future__ import annotations
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, x: int):
+        self.val = x
+        self.next: Optional[ListNode] = None
+
 
 class Solution:
-    def hasCycle(self, head: ListNode) -> bool:
-        if not head:
-            return False
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
         slow = fast = head
-        while fast.next and fast.next.next:
+        while slow and fast and fast.next:
             slow = slow.next
             fast = fast.next.next
             if slow == fast:
