d5cb0fc277db5b44d666fc3d5cc7f1598c3b934a
Satoshi Kamiyama <55924931+MtDeity@users.noreply.github.com>
2021-06-30 21:14:22 +0900

    75. Sort Colors

---
+++ Sort/sort_colors.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        left = cur = 0
+        right = len(nums) - 1
+        while cur <= right:
+            if nums[cur] == 0:
+                nums[left], nums[cur] = nums[cur], nums[left]
+                left += 1
+                cur += 1
+            elif nums[cur] == 1:
+                cur += 1
+            else:
+                nums[cur], nums[right] = nums[right], nums[cur]
+                right -= 1
