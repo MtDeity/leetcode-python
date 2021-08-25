9fca6cc4697325ca49187a11403996a99086c529
Satoshi Kamiyama <55924931+MtDeity@users.noreply.github.com>
2021-08-25 19:12:31 +0900

    633. Sum of Square Numbers

---
+++ Math/sum_of_square_numbers.py
@@ -0,0 +1,9 @@
+class Solution:
+    def judgeSquareSum(self, c: int) -> bool:
+        a = 0
+        while a ** 2 <= c:
+            b = (c - a ** 2) ** 0.5
+            if b == int(b):
+                return True
+            a += 1
+        return False
