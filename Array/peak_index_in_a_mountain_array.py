be77ab68ef27d4b21e2513c014cea159f8e81c28
Satoshi Kamiyama <55924931+MtDeity@users.noreply.github.com>
2021-09-03 01:39:23 +0900

    852. Peak Index in a Mountain Array

--- Array/peak_index_in_a_mountain_array.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def peakIndexInMountainArray(self, arr: list[int]) -> int:
-        idx = mx = 0
-        for i, num in enumerate(arr):
-            if num > mx:
-                mx = num
-                idx = i
-        return idx
