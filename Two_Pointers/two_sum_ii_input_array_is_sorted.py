b9578d2654e668d0ae6fc0f31448aa8cb8f472ab
Satoshi Kamiyama <55924931+MtDeity@users.noreply.github.com>
2021-09-22 20:13:35 +0900

    167. Two Sum II - Input array is sorted

---
+++ Two_Pointers/two_sum_ii_input_array_is_sorted.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, numbers: list[int], target: int) -> list[int]:
+        s = 0
+        e = len(numbers) - 1
+        while s < e:
+            if numbers[s] + numbers[e] == target:
+                return [s + 1, e + 1]
+            elif numbers[s] + numbers[e] < target:
+                s += 1
+            else:
+                e -= 1
+        return []
