1d470982a72aa644eb66eae3dfc55b613213864d
Satoshi Kamiyama <55924931+MtDeity@users.noreply.github.com>
2021-09-22 09:50:01 +0900

    283. Move Zeroes

--- Two_Pointers/move_zeroes.py
+++ Two_Pointers/move_zeroes.py
@@ -1,18 +1,7 @@
-from typing import List
-
-
 class Solution:
-    def moveZeroes(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        if not nums or len(nums) < 2:
-            return
-        i = 0
-        for num in nums:
-            if num != 0:
-                nums[i] = num
-                i += 1
-        while i < len(nums):
-            nums[i] = 0
-            i += 1
+    def moveZeroes(self, nums: list[int]) -> None:
+        z = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[i], nums[z] = nums[z], nums[i]
+                z += 1
