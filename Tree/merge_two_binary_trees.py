d818b7d04ff902f79c30105a662510d27c30cd5f
Satoshi Kamiyama <55924931+MtDeity@users.noreply.github.com>
2021-09-27 21:34:25 +0900

    617. Merge Two Binary Trees

--- Tree/merge_two_binary_trees.py
+++ Tree/merge_two_binary_trees.py
@@ -1,18 +1,27 @@
-# Definition for a binary tree node.
+from __future__ import annotations
+from typing import Optional
+
+
 class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
+    def __init__(self,
+                 val: int = 0,
+                 left: Optional[TreeNode] = None,
+                 right: Optional[TreeNode] = None):
         self.val = val
         self.left = left
         self.right = right
 
 
 class Solution:
-    def mergeTrees(self, t1: TreeNode, t2: TreeNode) -> TreeNode:
-        if not t1:
-            return t2
-        elif not t2:
-            return t1
-        t1.val += t2.val
-        t1.left = self.mergeTrees(t1.left, t2.left)
-        t1.right = self.mergeTrees(t1.right, t2.right)
-        return t1
+    def mergeTrees(
+            self,
+            root1: Optional[TreeNode],
+            root2: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root1:
+            return root2
+        elif not root2:
+            return root1
+        root1.val += root2.val
+        root1.left = self.mergeTrees(root1.left, root2.left)
+        root1.right = self.mergeTrees(root1.right, root2.right)
+        return root1
